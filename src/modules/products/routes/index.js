24ec722a5c96533235e989b5efe290f49c2160ae
NOI <onyedikachinwosu@rocketmail.com>
2019-05-08 11:33:49 +0100

    Finalize code structure

--- src/modules/products/routes/index.js
+++ src/modules/products/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const expressValidator = require("express-joi-validator");
 const ctrlAdmin = require("../index");
-const validateInput = require("../policies/");
+const validateInput = require("../policies");
 const {
   catchErrors,
   verifyToken,
